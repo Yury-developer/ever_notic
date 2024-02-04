6be0251243ad8cb3a6b12f81865c4e9df757f7b2
Yury Lapitski _home_PC <291056708@mail.ru>
2024-02-04 12:09:38 +0300

    The first commit. Working version for lesson 1, prototype of the father: https://youtu.be/7qvwtlnxVH8?si=VCJN1B1J6_oTxBQJ

---
+++ src/main/java/ru/evernotic/EverNoticApplication.java
@@ -0,0 +1,13 @@
+package ru.evernotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverNoticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverNoticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/evernotic/EverNoticApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evernotic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverNoticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
